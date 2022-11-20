984a5652d8bd6b1317fcfd6f6d491af4d363251c
aerialcombat <djkim24601@gmail.com>
2022-11-21 07:42:50 +0900

    Update TagListView.swift

--- TagListView/TagListView.swift
+++ TagListView/TagListView.swift
@@ -121,21 +121,13 @@ open class TagListView: UIView {
             rearrangeViews()
         }
     }
-
-    @IBInspectable open dynamic var minWidth: CGFloat = 0 {
-        didSet {
-            rearrangeViews()
-        }
-    }
     
     @objc public enum Alignment: Int {
         case left
         case center
         case right
-        case leading
-        case trailing
     }
-    @IBInspectable open var alignment: Alignment = .leading {
+    @IBInspectable open var alignment: Alignment = .left {
         didSet {
             rearrangeViews()
         }
@@ -195,6 +187,15 @@ open class TagListView: UIView {
             }
         }
     }
+
+    /// The desired (maximum) width of the whole view. This needs to be set in cases where
+    /// the `intrinsicContentSize` should to be calculated before or without the view being
+    /// previously layouted. For example when using `systemLayoutSizeFitting:`.
+    @IBInspectable open dynamic var preferredLayoutWidth: CGFloat = -1 {
+        didSet {
+            rearrangeViews()
+        }
+    }
     
     @objc open dynamic var textFont: UIFont = .systemFont(ofSize: 12) {
         didSet {
@@ -239,62 +240,32 @@ open class TagListView: UIView {
         }
         rowViews.removeAll(keepingCapacity: true)
 
-        var isRtl: Bool = false
-        
-        if #available(iOS 10.0, tvOS 10.0, *) {
-            isRtl = effectiveUserInterfaceLayoutDirection == .rightToLeft
-        }
-        else if #available(iOS 9.0, *) {
-            isRtl = UIView.userInterfaceLayoutDirection(for: semanticContentAttribute) == .rightToLeft
-        }
-        else if let shared = UIApplication.value(forKey: "sharedApplication") as? UIApplication {
-            isRtl = shared.userInterfaceLayoutDirection == .leftToRight
-        }
-        
-        var alignment = self.alignment
-        
-        if alignment == .leading {
-            alignment = isRtl ? .right : .left
-        }
-        else if alignment == .trailing {
-            alignment = isRtl ? .left : .right
-        }
+        let layoutWidth = preferredLayoutWidth >= 0 ? preferredLayoutWidth : frame.width
         
         var currentRow = 0
         var currentRowView: UIView!
         var currentRowTagCount = 0
         var currentRowWidth: CGFloat = 0
-        let frameWidth = frame.width
-        
-        let directionTransform = isRtl
-            ? CGAffineTransform(scaleX: -1.0, y: 1.0)
-            : CGAffineTransform.identity
-        
         for (index, tagView) in tagViews.enumerated() {
             tagView.frame.size = tagView.intrinsicContentSize
             tagViewHeight = tagView.frame.height
             
-            if currentRowTagCount == 0 || currentRowWidth + tagView.frame.width > frameWidth {
+            if currentRowTagCount == 0 || currentRowWidth + tagView.frame.width > layoutWidth {
                 currentRow += 1
                 currentRowWidth = 0
                 currentRowTagCount = 0
                 currentRowView = UIView()
-                currentRowView.transform = directionTransform
                 currentRowView.frame.origin.y = CGFloat(currentRow - 1) * (tagViewHeight + marginY)
                 
                 rowViews.append(currentRowView)
                 addSubview(currentRowView)
 
-                tagView.frame.size.width = min(tagView.frame.size.width, frameWidth)
+                tagView.frame.size.width = min(tagView.frame.size.width, layoutWidth)
             }
             
             let tagBackgroundView = tagBackgroundViews[index]
-            tagBackgroundView.transform = directionTransform
-            tagBackgroundView.frame.origin = CGPoint(
-                x: currentRowWidth,
-                y: 0)
+            tagBackgroundView.frame.origin = CGPoint(x: currentRowWidth, y: 0)
             tagBackgroundView.frame.size = tagView.bounds.size
-            tagView.frame.size.width = max(minWidth, tagView.frame.size.width)
             tagBackgroundView.layer.shadowColor = shadowColor.cgColor
             tagBackgroundView.layer.shadowPath = UIBezierPath(roundedRect: tagBackgroundView.bounds, cornerRadius: cornerRadius).cgPath
             tagBackgroundView.layer.shadowOffset = shadowOffset
@@ -307,14 +278,12 @@ open class TagListView: UIView {
             currentRowWidth += tagView.frame.width + marginX
             
             switch alignment {
-            case .leading: fallthrough // switch must be exahutive
             case .left:
                 currentRowView.frame.origin.x = 0
             case .center:
-                currentRowView.frame.origin.x = (frameWidth - (currentRowWidth - marginX)) / 2
-            case .trailing: fallthrough // switch must be exahutive
+                currentRowView.frame.origin.x = (layoutWidth - (currentRowWidth - marginX)) / 2
             case .right:
-                currentRowView.frame.origin.x = frameWidth - (currentRowWidth - marginX)
+                currentRowView.frame.origin.x = layoutWidth - (currentRowWidth - marginX)
             }
             currentRowView.frame.size.width = currentRowWidth
             currentRowView.frame.size.height = max(tagViewHeight, currentRowView.frame.height)
@@ -331,7 +300,11 @@ open class TagListView: UIView {
         if rows > 0 {
             height -= marginY
         }
-        return CGSize(width: frame.width, height: height)
+
+        // Take the width of the widest rowView to determine the intrinsic content width
+        let width = rowViews.map({ $0.frame.width }).sorted().last ?? 0.0
+
+        return CGSize(width: width, height: height)
     }
     
     private func createNewTagView(_ title: String) -> TagView {
@@ -388,11 +361,9 @@ open class TagListView: UIView {
     }
     
     @discardableResult
-    open func addTagViews(_ tagViewList: [TagView]) -> [TagView] {
-        defer { rearrangeViews() }
-        tagViewList.forEach {
-            tagViews.append($0)
-            tagBackgroundViews.append(UIView(frame: $0.bounds))
+    open func addTagViews(_ tagViews: [TagView]) -> [TagView] {
+        tagViews.forEach {
+            addTagView($0)
         }
         return tagViews
     }
@@ -424,7 +395,7 @@ open class TagListView: UIView {
         defer { rearrangeViews() }
         
         tagView.removeFromSuperview()
-        if let index = tagViews.firstIndex(of: tagView) {
+        if let index = tagViews.index(of: tagView) {
             tagViews.remove(at: index)
             tagBackgroundViews.remove(at: index)
         }
